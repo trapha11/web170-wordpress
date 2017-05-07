ad34d6eac470b0efc8914b861a59159bd465065b
Trang Pham <tpham079@seattlecentral.edu>
2017-05-06 22:52:11 -0700

    05) Create Your Theme from Your Template
    Create the 1) Index (index.php), 2) Main Styles (style.css), 3) Functions (functions.php) and 4) The Screenshot (screenshot.png) files from template files.
    Add the 1) bloginfo, 2) body_class, 3) wp_head, 4) wp_footer functions to theme.
    Create The Loop within the content area of theme that includes the 1) The Title, 2) The Permalink and 3) The Content functions.

---
+++ template/functions.php
@@ -0,0 +1,16 @@
+<?php 
+
+/*
+
+Theme Name: Wordpress Spring 2017 Section 02 Demo
+Author: Trang Pham  
+Author URI: http://tidbitwebdesign.com
+Description: This is the demo theme for the WEB170 section 02 class foor Spring 2017 Quarter.
+Version: 1.0
+
+*/
+
+//register Navigation Menu
+register_nav_menus(array('main-menu' => __('Main Menu tada!'))); //slug pass through main menu slug and the title doesn't have to be matched
+
+?>
